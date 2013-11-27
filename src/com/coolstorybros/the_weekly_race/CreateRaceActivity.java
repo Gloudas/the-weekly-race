6a662c9c1f8d141a436847ece5613aae0631d10d
Anirudh <anirudh.garg0817@gmail.com>
2013-11-26 22:53:34 -0800

    Better error handling and tiny design changes

--- src/com/coolstorybros/the_weekly_race/CreateRaceActivity.java
+++ src/com/coolstorybros/the_weekly_race/CreateRaceActivity.java
@@ -1,7 +1,11 @@
 package com.coolstorybros.the_weekly_race;
 
 import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +14,11 @@ import android.widget.EditText;
 
 public class CreateRaceActivity extends Activity {
 	
+	final Context context = this;
 	Button mCreateRaceButton;
+	
+	boolean emptyFlag = false;
+	boolean invalidDateFlag = false;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,46 +33,118 @@ public class CreateRaceActivity extends Activity {
      * @param v Button that was clicked
      */
     public void createRaceButtonClicked(View v) {
+    	emptyFlag = false;
+    	invalidDateFlag = false;
+    	
     	final EditText titleField = (EditText) findViewById(R.id.EditTextRaceTitle);  
     	String title = titleField.getText().toString();
+    	if (title.equals("")){
+    		emptyFlag = true;
+    	}
 
     	final EditText locationField = (EditText) findViewById(R.id.EditTextRaceLocation);  
     	String location = locationField.getText().toString();  
-
+    	if (location.equals("")){
+    		emptyFlag = true;
+    	}
+    	
     	final EditText detailsField = (EditText) findViewById(R.id.EditTextRaceDetails);  
     	String details = detailsField.getText().toString();  
+    	if (details.equals("")){
+    		emptyFlag = true;
+    	}
     	
+    	int startmonth = 0, startday = 0, startyear = 0;
     	final EditText startDateField = (EditText) findViewById(R.id.EditTextRaceStartDate);  
     	String startdatestring = startDateField.getText().toString();
-    	String[] startdatevals = startdatestring.split("[/]");
-    	int startmonth = Integer.parseInt(startdatevals[0]) - 1; //January is 0
-    	int startday = Integer.parseInt(startdatevals[1]);
-    	int startyear = Integer.parseInt(startdatevals[2]);
+    	if (startdatestring.equals("")){
+    		emptyFlag = true;
+    	} else {
+    		String[] startdatevals = startdatestring.split("[/]");
+    		if (startdatevals.length == 3) {
+    			startmonth = Integer.parseInt(startdatevals[0]) - 1; //January is 0
+    			startday = Integer.parseInt(startdatevals[1]);
+    			startyear = Integer.parseInt(startdatevals[2]);
+    		} else {
+    	    	invalidDateFlag = true;
+    		}
+    	}
     	
+    	int endmonth = 0, endday = 0, endyear = 0;
     	final EditText endDateField = (EditText) findViewById(R.id.EditTextRaceEndDate);  
     	String enddatestring = endDateField.getText().toString();
-    	String[] enddatevals = enddatestring.split("[/]");
-    	int endmonth = Integer.parseInt(enddatevals[0]) - 1; //January is 0
-    	int endday = Integer.parseInt(enddatevals[1]);
-    	int endyear = Integer.parseInt(enddatevals[2]);
-    	
+    	if (enddatestring.equals("")){
+    		emptyFlag = true;
+    	} else {
+        	String[] enddatevals = enddatestring.split("[/]");
+    		if (enddatevals.length == 3) {
+    			endmonth = Integer.parseInt(enddatevals[0]) - 1; //January is 0
+    			endday = Integer.parseInt(enddatevals[1]);
+    			endyear = Integer.parseInt(enddatevals[2]);
+    		} else {
+    	    	invalidDateFlag = true;
+    		}
+    	}
 
     	final EditText prizeField = (EditText) findViewById(R.id.EditTextRacePrizes);  
     	String prize = prizeField.getText().toString();
+    	if (title.equals("")){
+    		emptyFlag = true;
+    	}
     	
     	final EditText winnersField = (EditText) findViewById(R.id.EditTextRaceWinners);  
-    	int winners = Integer.parseInt(winnersField.getText().toString());
-
-    	Calendar startdate = Calendar.getInstance();
-    	startdate.set(startyear, startmonth, startday);
+    	String winnersString = winnersField.getText().toString();
+    	int winners = 0;
+    	if (winnersString.equals("")){
+    		emptyFlag = true;
+    	} else {
+    		winners = Integer.parseInt(winnersString);
+    	}
     	
-    	Calendar enddate = Calendar.getInstance();
-    	enddate.set(endyear, endmonth, endday);
+    	if (emptyFlag){
+    		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+    		alertDialogBuilder.setTitle("Attention!");
+     
+    		alertDialogBuilder
+    			.setMessage("Please make sure you have filled all the fields. All of the information is required!")
+    			.setNegativeButton("Go back!", new DialogInterface.OnClickListener() {
+    					public void onClick(DialogInterface dialog,int id) {
+    						// if this button is clicked, close
+    						// current activity
+    						dialog.cancel();
+    					}
+    			});
+    				
+    		AlertDialog alertDialog = alertDialogBuilder.create();
+    		alertDialog.show();
+    	} else if (invalidDateFlag) {
+    		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+    		alertDialogBuilder.setTitle("Attention!");
+     
+    		alertDialogBuilder
+    			.setMessage("Please make sure the dates that you entered are correct!")
+    			.setNegativeButton("Go back!", new DialogInterface.OnClickListener() {
+    					public void onClick(DialogInterface dialog,int id) {
+    						// if this button is clicked, close
+    						// current activity
+    						dialog.cancel();
+    					}
+    			});
+    				
+    		AlertDialog alertDialog = alertDialogBuilder.create();
+    		alertDialog.show();
+    	} else {
+    		Calendar startdate = Calendar.getInstance();
+    		startdate.set(startyear, startmonth, startday);
     	
-    	Race newRace = new Race(title, location, details, startdate, enddate, prize, winners);
-    	ManageRaceActivity.setRace(newRace);
+    		Calendar enddate = Calendar.getInstance();
+    		enddate.set(endyear, endmonth, endday);
+    		
+    		Race newRace = new Race(title, location, details, startdate, enddate, prize, winners);
+    		ManageRaceActivity.setRace(newRace);
     	
-    	Intent intent = new Intent(this, RaceDetailsActivity.class);
-        startActivity(intent);
+    		Intent intent = new Intent(this, RaceDetailsActivity.class);
+    		startActivity(intent);
+    	}
     }
 }
