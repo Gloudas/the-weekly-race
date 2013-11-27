77aa3debb4024e6ebf72e67f438fd79f3c6b2a16
vnedyavila <vivek.nedyavila@gmail.com>
2013-11-26 16:31:50 -0800

    CreateRace partial functionality added

--- src/com/coolstorybros/the_weekly_race/CreateRaceActivity.java
+++ src/com/coolstorybros/the_weekly_race/CreateRaceActivity.java
@@ -1,13 +1,58 @@
 package com.coolstorybros.the_weekly_race;
 
+import java.util.Calendar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateRaceActivity extends Activity {
-
+	
+	Button mCreateRaceButton;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_race);
+        
+        mCreateRaceButton = (Button) findViewById(R.id.button_create_race);
+    }
+    
+    /**
+     * Pressed when the "Create new race" button is clicked
+     * @param v Button that was clicked
+     */
+    public void createRaceButtonClicked(View v) {
+    	final EditText titleField = (EditText) findViewById(R.id.EditTextRaceTitle);  
+    	String title = titleField.getText().toString();
+    	
+    	final EditText detailsField = (EditText) findViewById(R.id.EditTextRaceDetails);  
+    	String details = detailsField.getText().toString();  
+    	
+    	final EditText dateField = (EditText) findViewById(R.id.EditTextRaceDate);  
+    	String datestring = dateField.getText().toString();
+    	String[] datevals = datestring.split("[/]");
+    	int month = Integer.parseInt(datevals[0]) - 1; //January is 0
+    	int day = Integer.parseInt(datevals[1]);
+    	int year = Integer.parseInt(datevals[2]);
+    	
+    	final EditText pointsField = (EditText) findViewById(R.id.EditTextRacePoints);  
+    	int points = Integer.parseInt(pointsField.getText().toString());
+    	
+    	final EditText prizeField = (EditText) findViewById(R.id.EditTextRacePrizes);  
+    	String prize = prizeField.getText().toString();
+    	
+    	final EditText winnersField = (EditText) findViewById(R.id.EditTextRaceWinners);  
+    	int winners = Integer.parseInt(winnersField.getText().toString());
+
+    	Calendar date = Calendar.getInstance();
+    	date.set(year, month, day);
+    	
+    	Race newRace = new Race(title, details, date, points, prize, winners);
+
+    	Intent intent = new Intent(this, ManageRaceActivity.class);
+        startActivity(intent);
     }
 }
--- src/com/coolstorybros/the_weekly_race/Race.java
+++ src/com/coolstorybros/the_weekly_race/Race.java
@@ -1,18 +1,76 @@
 package com.coolstorybros.the_weekly_race;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Describes the properties of a Weekly Race
  */
 public class Race {
 
     private String mTitle;
-    private String mDescription;
-    // TODO - add more race variables as we need
+    private String mDetails;
+    private Calendar mStartDate;
+    private int mPoints;
+    private String mPrize;
+    private int mNumWinners;
 
-    public Race() {
-        mTitle = "Placeholder Title";
-        mDescription = "Placeholder Description";
+    public Race(String title, String details, Calendar date, int points, String prize, int numWinners) {
+        mTitle = title;
+        mDetails = details;
+        mStartDate = date;
+        mPoints = points;
+        mPrize = prize;
+        mNumWinners = numWinners;
     }
-
-    // TODO add getters/setters for all the variables
+    
+    public String getTitle()
+    {
+    	return mTitle;
+    }
+    public String getDetails()
+    {
+    	return mDetails;
+    }
+    public String getPrize()
+    {
+    	return mPrize;
+    }
+    public int getPoints()
+    {
+    	return mPoints;
+    }
+    public int getNumWinners()
+    {
+    	return mNumWinners;
+    }
+    public Calendar getStartDate()
+    {
+    	return mStartDate;
+    }
+    
+    public void setTitle(String newTitle)
+    {
+    	mTitle = newTitle;
+    }
+    public void setDetails(String newDetails)
+    {
+    	mDetails = newDetails;
+    }
+    public void setPrize(String newPrize)
+    {
+    	mPrize = newPrize;
+    }
+    public void setPoints(int newPoints)
+    {
+    	mPoints = newPoints;
+    }
+    public void setNumWinners(int newNumWinners)
+    {
+    	mNumWinners = newNumWinners;
+    }
+    public void setStartDate(Calendar newStartDate)
+    {
+    	mStartDate = newStartDate;
+    }
 }
